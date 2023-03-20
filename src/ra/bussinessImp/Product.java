1ff42241179e29a66fc2c553c9dd711e3f6078af
Sonnm90 <kenkon42@gmail.com>
2023-03-20 11:10:42 +0700

    up

---
+++ src/ra/bussinessImp/Product.java
@@ -0,0 +1,172 @@
+package ra.bussinessImp;
+
+import ra.bussiness.IProduct;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Product  implements IProduct, Comparable<Product> {
+    private int productId;
+    private String productName;
+    private String title;
+    private String description;
+    private float importPrice;
+    private float exportPrice;
+    private float interest;
+    private boolean productStatus;
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, String title, String description, float importPrice, float exportPrice, float interest, boolean productStatus) {
+        this.productId = productId;
+        this.productName = productName;
+        this.title = title;
+        this.description = description;
+        this.importPrice = importPrice;
+        this.exportPrice = exportPrice;
+        this.interest = interest;
+        this.productStatus = productStatus;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getImportPrice() {
+        return importPrice;
+    }
+
+    public void setImportPrice(float importPrice) {
+        this.importPrice = importPrice;
+    }
+
+    public float getExportPrice() {
+        return exportPrice;
+    }
+
+    public void setExportPrice(float exportPrice) {
+        this.exportPrice = exportPrice;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+
+    public void setInterest(float interest) {
+        this.interest = interest;
+    }
+
+    public boolean isProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(boolean productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    @Override
+    public void inputData() {
+        Scanner sc=new Scanner(System.in);
+
+        // Nhap Id san pham:
+        System.out.println("Nhap Id san pham");
+        this.productId = sc.nextInt();
+
+
+        //Nhap ten san pham:
+        sc.nextLine();
+        System.out.println("Nhap ten san pham:");
+        this.productName=sc.nextLine();
+
+
+        //Nhap tieu de cho san pham:
+        System.out.println("Nhap tieu de gioi thieu san pham:");
+        this.title=sc.nextLine();
+//        sc.nextLine();
+
+        //Nhap mo ta san pham:
+        System.out.println("Nhap mo ta cho san pham:");
+        this.description=sc.nextLine();
+//        sc.nextLine();
+
+        //Nhap gia nhap vao:
+        System.out.println("Nhap gia nhap vao cua san pham ($):");
+        this.importPrice=sc.nextFloat();
+//        sc.next();
+
+        //Nhap gia xuat ra:
+        System.out.println("Nhap gia xuat ra cua san pham ($):");
+        this.exportPrice=sc.nextFloat();
+//        sc.next();
+
+        //Nhap tinh trang san pham:
+        System.out.println("Nhap trang thai san pham:");
+        this.productStatus=sc.nextBoolean();
+
+        //Tinh loi nhuan:
+        this.interest = this.exportPrice-this.importPrice;
+
+    }
+
+    @Override
+    public void displayData() {
+        System.out.println("Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", importPrice=" + importPrice +
+                ", exportPrice=" + exportPrice +
+                ", interest=" + interest +
+                ", productStatus=" + productStatus +
+                '}');
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", importPrice=" + importPrice +
+                ", exportPrice=" + exportPrice +
+                ", interest=" + interest +
+                ", productStatus=" + productStatus +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Product product) {
+        return (int) (this.interest-product.interest);
+    }
+}
---
+++ src/ra/run/ProductManagement.java
@@ -0,0 +1,97 @@
+package ra.run;
+
+import ra.bussinessImp.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProductManagement {
+    static int size;
+    static List<Product> products = new ArrayList<>();
+
+    public static void main(String[] args) {
+        boolean cont = true;
+        int choice;
+        Scanner sc = new Scanner(System.in);
+
+
+        do {
+            System.out.println("****************JAVA-HACKATHON-05-BASIC-MENU***************\n" +
+                    "1. Nhập số sản phẩm và nhập thông tin sản phẩm \n" +
+                    "2. Hiển thị thông tin các sản phẩm \n" +
+                    "3. Sắp xếp sản phẩm theo lợi nhuận tăng dần \n" +
+                    "4. Xóa sản phẩm theo mã sản phẩm \n" +
+                    "5. Tìm kiếm sản phẩm theo tên sản phẩm \n" +
+                    "6. Thay đổi trạng thái của sản phẩm theo mã sản phẩm \n" +
+                    "7. Thoát ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Nhap so luong san pham");
+                    size = sc.nextInt();
+//                   products= new Product[size];
+                    for (int i = 0; i < size; i++) {
+                        System.out.println("Nhap thong tin san pham thu" + (i + 1) + " :");
+                        products.add(new Product());
+                        products.get(i).inputData();
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < products.size(); i++) {
+
+                        products.get(i).displayData();
+                    }
+                    break;
+                case 3:
+                    Collections.sort(products);
+//
+                    System.out.println("Mang sau khi sap xep tang dan loi nhuan: "+products);
+                    break;
+                case 4:
+                    System.out.println("Nhap ma san pham can xoa:");
+                    int removeId= sc.nextInt();
+                    System.out.println("Mang truoc khi xoa: "+products);
+                    for (int i = 0; i < products.size(); i++) {
+                       if(products.get(i).getProductId()==removeId){
+                           products.remove(i);
+                       }
+                    }
+                    System.out.println("Mang sau khi xoa: "+products);
+                    break;
+                case 5:
+                    sc.nextLine();
+                    System.out.println("Nhap ten san pham can tim kiem:");
+                    String findProductName= sc.nextLine();
+                    for (int i = 0; i < products.size(); i++) {
+                        if(products.get(i).getProductName().contains(findProductName)){
+                            products.get(i).displayData();
+                        }
+                    }
+                    break;
+                case 6:
+                    System.out.println("Nhap ma san pham can thay doi trang thai:");
+                    int changeStatus= sc.nextInt();
+                    for (int i = 0; i < products.size(); i++) {
+                        if(products.get(i).getProductId()==changeStatus){
+                            System.out.println("san pham truoc khi thay doi trang thai");
+                            products.get(i).displayData();
+                           boolean check= products.get(i).isProductStatus();
+                           if (check){
+                               products.get(i).setProductStatus(false);
+                           } else
+                               products.get(i).setProductStatus(true);
+                            System.out.println("san pham sau khi thay doi trang thai");
+                            products.get(i).displayData();
+                        }
+                    }
+                    break;
+                case 7:
+                    cont = false;
+            }
+
+
+        } while (cont);
+    }
+}
